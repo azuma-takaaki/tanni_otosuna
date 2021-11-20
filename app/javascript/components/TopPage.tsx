e68178cdd21922fbc4658d9a170d550bbc5ea386
azuma-takaaki <azuma.takaaki.1000@gmail.com>
2021-11-20 19:25:55 +0900

    add start page #1

---
+++ app/javascript/components/TopPage.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+const TopPage = ({message})  => {
+  return (
+    <>
+      <h1>単位落とすな</h1>
+      <button>スタート</button>
+      <button>遊び方</button>
+    </>
+  );
+};
+export default TopPage;
