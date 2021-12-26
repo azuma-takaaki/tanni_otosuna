cac0a234e7bf11ed0dd2bf6ff527aa4683d80d51
azuma-takaaki <azuma.takaaki.1000@gmail.com>
2021-12-26 15:41:05 +0900

    Add ranking page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  get '/ranking/show', to: 'ranking#show'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
