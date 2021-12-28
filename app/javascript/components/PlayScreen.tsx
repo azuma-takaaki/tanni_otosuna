fab8461d1a2c9b2736ed75188c35846137d98356
azuma-takaaki <azuma.takaaki.1000@gmail.com>
2021-12-28 19:10:50 +0900

    Draw player image to canvas

--- app/javascript/components/PlayScreen.tsx
+++ app/javascript/components/PlayScreen.tsx
@@ -1,6 +1,29 @@
 import * as React from 'react';
 import {useState, useEffect, useRef} from 'react';
 
+document.addEventListener('keydown', (event) => {
+    var keyName = event.key;
+
+    if (event.ctrlKey) {
+      console.log(`keydown:Ctrl + ${keyName}`);
+    } else if (event.shiftKey) {
+      console.log(`keydown:Shift + ${keyName}`);
+    } else {
+      console.log(`keydown:${keyName}`);
+    }
+  });
+
+  document.addEventListener('keypress', (event) => {
+    var keyName = event.key;
+
+    if (event.ctrlKey) {
+      console.log(`keypress:Ctrl + ${keyName}`);
+    } else if (event.shiftKey) {
+      console.log(`keypress:Shift + ${keyName}`);
+    } else {
+      console.log(`keypress:${keyName}`);
+    }
+  });
 
 const PlayScreen = ({})  => {
   const [screenWidth, setScreenWidth] = useState<number>(400)
@@ -17,12 +40,40 @@ const PlayScreen = ({})  => {
 
   useEffect(() => {
     const ctx: CanvasRenderingContext2D = getContext();
-    ctx.fillRect(playerX, playerY, 100, 100);
+    const player_image = new Image();
+    player_image.src = "/assets/player.png";
+    player_image.onload = () => {
+        ctx.drawImage(player_image, playerX, playerY)
+    };
     ctx.save();
   })
+
+  const movePlayer = (delta_x) => {
+    const ctx: CanvasRenderingContext2D = getContext();
+    const player_image = new Image();
+    player_image.src = "/assets/player.png";
+    player_image.onload = () => {
+        ctx.drawImage(player_image, playerX, playerY)
+    };
+    ctx.save();
+    setPlayerX(playerX + delta_x)
+  }
+
   return (
     <>
       <canvas className="canvas" ref={canvasRef} width={screenWidth} height={screenHeight}/>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick = {()=>movePlayer(-10)}
+      >
+          ←
+      </button>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick = {()=>movePlayer(10)}
+      >
+          →
+      </button>
     </>
   );
 };
