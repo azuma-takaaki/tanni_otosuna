307544f2aa51a499195e11df1461bf13cfb755b9
azuma-takaaki <azuma.takaaki.1000@gmail.com>
2021-12-29 22:39:13 +0900

    WIP

--- app/javascript/components/PlayScreen.tsx
+++ app/javascript/components/PlayScreen.tsx
@@ -1,36 +1,19 @@
 import * as React from 'react';
 import {useState, useEffect, useRef} from 'react';
-
-document.addEventListener('keydown', (event) => {
-    var keyName = event.key;
-
-    if (event.ctrlKey) {
-      console.log(`keydown:Ctrl + ${keyName}`);
-    } else if (event.shiftKey) {
-      console.log(`keydown:Shift + ${keyName}`);
-    } else {
-      console.log(`keydown:${keyName}`);
-    }
-  });
-
-  document.addEventListener('keypress', (event) => {
-    var keyName = event.key;
-
-    if (event.ctrlKey) {
-      console.log(`keypress:Ctrl + ${keyName}`);
-    } else if (event.shiftKey) {
-      console.log(`keypress:Shift + ${keyName}`);
-    } else {
-      console.log(`keypress:${keyName}`);
-    }
-  });
+import {ScreenManeger} from './ScreenManeger';
 
 const PlayScreen = ({})  => {
+  const count = 0;
+  let screenManeger;
+  let playerX = 0;
+  let playerY = 0;
   const [screenWidth, setScreenWidth] = useState<number>(400)
   const [screenHeight, setScreenHeight] = useState(600)
-  const [playerX, setPlayerX] = useState(0)
-  const [playerY, setPlayerY] = useState(0)
+  const [amountPlayerMove, setAmountPlayerMove] = useState(10)
   const canvasRef = useRef(null);
+  const player_image = new Image();
+  player_image.src = "/assets/player.png";
+  
 
   const getContext = (): CanvasRenderingContext2D => {
     const canvas: any = canvasRef.current;
@@ -40,37 +23,69 @@ const PlayScreen = ({})  => {
 
   useEffect(() => {
     const ctx: CanvasRenderingContext2D = getContext();
-    const player_image = new Image();
-    player_image.src = "/assets/player.png";
-    player_image.onload = () => {
-        ctx.drawImage(player_image, playerX, playerY)
-    };
+
     ctx.save();
+
+    const assets = {
+      player: "/assets/player.png"
+    };
+
+    screenManeger = new ScreenManeger({
+      canvas: canvasRef.current,
+      canvas_context: getContext(),
+      assets: assets,
+      fps: 30
+    })
+
+    console.log("useEffect() is called")
+    screenManeger.start()
   })
 
-  const movePlayer = (delta_x) => {
+
+
+  /*
+  const handleKeyDown = (event: KeyboardEvent) => {
+    const keyName = event.key;
+    if (keyName == "ArrowRight"){
+      movePlayer(amountPlayerMove)
+      console.log("ArrowRight");
+    }else if (keyName == "ArrowLeft"){
+      movePlayer(-amountPlayerMove)
+      console.log("ArrowLeft");
+    }
+  }
+
+  useEffect(() => {
     const ctx: CanvasRenderingContext2D = getContext();
-    const player_image = new Image();
-    player_image.src = "/assets/player.png";
     player_image.onload = () => {
         ctx.drawImage(player_image, playerX, playerY)
     };
     ctx.save();
-    setPlayerX(playerX + delta_x)
+    document.addEventListener('keydown', handleKeyDown, false)
+  }, [])
+
+  const movePlayer = (delta_x) => {
+    playerX += delta_x
+    const ctx: CanvasRenderingContext2D = getContext();
+    ctx.clearRect(0, 0, screenWidth, screenHeight);
+    addEventListener('load', function() {
+      ctx.drawImage(player_image, playerX, playerY)
+    }, false);
+    ctx.save();
   }
+  */
 
   return (
     <>
       <canvas className="canvas" ref={canvasRef} width={screenWidth} height={screenHeight}/>
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick = {()=>movePlayer(-10)}
       >
           ←
       </button>
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick = {()=>movePlayer(10)}
+        onClick={() => screenManeger.start()}
       >
           →
       </button>
---
+++ app/javascript/components/ScreenManeger.tsx
@@ -0,0 +1,76 @@
+export class ScreenManeger {
+  canvas
+  fps
+  ctx
+  assets
+  loadedAssets
+  timer
+  items
+  keyboard
+  player_image
+  constructor(opt){
+    this.canvas = opt.canvas;
+    // 描画用コンテキスト
+    this.ctx = opt.canvas.getContext('2d');
+    
+    // アセット（画像ファイルなど）
+    this.assets = opt.assets;
+    this.loadedAssets = {};
+    
+    // 描画速度（フレームレート）
+    this.fps = opt.fps || 30;
+
+    // 無限ループ用のtimer（start・stop時に使用）
+    this.timer;
+
+    // Canvasに描画するオブジェクトリスト
+    this.items = [];
+
+    // キーボード入力を保持する
+    this.keyboard = '';
+
+    this.player_image = new Image();
+    this.player_image.src = "/assets/player.png";
+  }
+
+  async start() {
+    console.log("start() is called")
+    //await this.loadAssets();
+    this.timer = setInterval(() => {
+      console.log("timer() is called")
+      this.render();
+    }, 1000 / this.fps);
+  }
+
+  /*
+  async loadAssets() {
+    const promises = Object.keys(this.assets).map(asset => {
+      return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.onload = () => { resolve }
+        img.onerror = err => { reject(err); }
+        img.src = this.assets[asset];
+        this.loadedAssets[asset] = img;
+      });
+    });
+
+    return Promise.all(promises);
+  }
+  */
+  /**
+   * Canvasをクリアし、オブジェクトを再描画する
+   */
+  render() {
+    // Canvasをクリアする
+    this.ctx.clearRect(0, 0, this.canvas.clientWidth, this.canvas.clientHeight);
+    this.ctx.drawImage(this.player_image, Math.floor( Math.random() * 100 ), 100)
+    this.items.forEach(a => {
+      // オブジェクトを再描画する
+      a.draw(this.ctx, this.loadedAssets);
+      // オブジェクトの状態を変更する
+      a.update(this.keyboard);
+    })
+
+
+  }
+}
