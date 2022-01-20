2c32b5217977beaa261aa6b342869e9c37df5941
azuma-takaaki <azuma.takaaki.1000@gmail.com>
2022-01-20 22:44:20 +0900

    WIP

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -21,11 +21,28 @@ class ResultsController < ApplicationController
 
     def get_title(result)
       title_list = title_list()
+      title_list.each do |t|
+        p t[:name]
+      end
+      p title_list.length
       point_type_list = ["tanni", "love", "business", "club"]
       point_type_list.each do |type|
-        title_list.each do |title|
-          unless result[type+"_point"]>=title[(type+"_range").to_sym][0] && result[type+"_point"]<=title[(type+"_range").to_sym][1]
-            title_list.delete(title)
+        p "==========================="
+        p type
+        p title_list.length
+        delete_index = []
+        title_list.each_with_index do |title, index|
+          p title[:name]
+          p title[(type+"_range").to_sym][0].to_s + " 〜 " + title[(type+"_range").to_sym][1].to_s + " : " + title[:name]
+          unless result[type+"_point"]>=title[(type+"_range").to_sym][0] && result[type+"_point"]<=title[(type+"_range").to_sym][1] then
+            delete_index.push index
+            p "↑ ×"
+          else
+            p "↑ ○"
+          end
+          delete_index.each do |i|
+            p title_list[i][:name] + "は削除"
+            title_list.delete_at i
           end
         end
       end
@@ -34,7 +51,7 @@ class ResultsController < ApplicationController
       elsif title_list.length == 1 then
         return title_list[0][:name]
       elsif title_list.length >= 1 then
-        return "複数マッチしました"
+        return title_list
       end
     end
 
@@ -44,46 +61,38 @@ class ResultsController < ApplicationController
         {
           name: "留年",
           tanni_range:    [0, 123],
-          love_range:     [0, infinite],
+          love_range:     [0, 99],
           business_range: [0, infinite],
           club_range:     [0, infinite],
         },
         {
-          name: "勝ち組一流サラリーマン",
-          tanni_range:    [124, infinite],
+          name: "ヒモ",
+          tanni_range:    [0, 123],
           love_range:     [100, infinite],
-          business_range: [100, infinite],
-          club_range:     [100, infinite],
+          business_range: [0, 49],
+          club_range:     [0, infinite],
         },
         {
-          name: "一流サラリーマン",
-          tanni_range:    [124, infinite],
-          love_range:     [0, 99],
-          business_range: [100, infinite],
-          club_range:     [100, infinite],
+          name: "就活浪人",
+          tanni_range:    [0, 123],
+          love_range:     [100, infinite],
+          business_range: [50, infinite],
+          club_range:     [0, infinite],
         },
         {
           name: "サラリーマン",
           tanni_range:    [124, infinite],
-          love_range:     [0, 100],
-          business_range: [50, 99],
-          club_range:     [50, infinite],
+          love_range:     [0, infinite],
+          business_range: [0, 99],
+          club_range:     [0, infinite],
         },
-      ]
-        # {
-        #   name: "フリーター",
-        #   tanni_range:    [0, infinite],
-        #   love_range:     [0, 100],
-        #   business_range: [0, 49],
-        #   club_range:     [0, infinite],
-        # },
-        # {
-        #   name: "ヒモ",
-        #   tanni_range:    [0, infinite],
-        #   love_range:     [100, infinite],
-        #   business_range: [0, 20],
-        #   club_range:     [0, infinite],
-        # },
+        {
+          name: "一流サラリーマン",
+          tanni_range:    [124, infinite],
+          love_range:     [0, infinite],
+          business_range: [100, infinite],
+          club_range:     [0, infinite],
+        },
         # {
         #   name: "ニート",
         #   tanni_range:    [0, 20],
@@ -92,11 +101,33 @@ class ResultsController < ApplicationController
         #   club_range:     [0, 20],
         # },
         # {
+        #   name: "一流サラリーマン",
+        #   tanni_range:    [124, infinite],
+        #   love_range:     [0, 99],
+        #   business_range: [100, infinite],
+        #   club_range:     [100, infinite],
+        # },
+        # {
+        #   name: "サラリーマン",
+        #   tanni_range:    [124, infinite],
+        #   love_range:     [0, 99],
+        #   business_range: [50, 99],
+        #   club_range:     [0, infinite],
+        # },
+        # {
+        #   name: "フリーター",
+        #   tanni_range:    [124, infinite],
+        #   love_range:     [0, 100],
+        #   business_range: [0, 49],
+        #   club_range:     [0, infinite],
+        # },
+        # {
         #   name: "大学院生",
         #   tanni_range:    [124, infinite],
         #   love_range:     [0, 20],
         #   business_range: [0, 20],
         #   club_range:     [0, infinite],
         # }
+      ]
     end
 end
