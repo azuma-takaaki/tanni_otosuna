4d9c47602d4d7a136a6bde0646a5c5f6a50de42b
azuma-takaaki <azuma.takaaki.1000@gmail.com>
2022-02-05 14:41:26 +0900

    Add point gauge

--- app/javascript/components/PlayScreen.tsx
+++ app/javascript/components/PlayScreen.tsx
@@ -17,14 +17,10 @@ const PlayScreen = ({})  => {
   return (
     <>
       <div>制限時間: <span id="time_limit">20</span></div>
-      <PointGauge/>
-      <div className="gauge_wrapper">
-        <div id="tanni_score_gauge"></div>
-        単位: <span id="tanni_score">0</span>
-      </div>
-      <div id="love_score_gauge">恋愛: <span id="love_score">0</span></div>
-      <div id="business_score_gauge">就活: <span id="business_score">0</span></div>
-      <div id="club_score_gauge">友情: <span id="club_score">0</span></div>
+      <PointGauge type="tanni"/>
+      <PointGauge type="love"/>
+      <PointGauge type="business"/>
+      <PointGauge type="club"/>
       <Canvas
         c_ref = {ref}
       />
